1a2aed4d807198681c17da595627c7acb75fec43
PJ Jones <pj.jones4000.com>
2022-03-24 15:53:12 -0500

    working on advanced section

--- server/server.js
+++ server/server.js
@@ -1,14 +1,46 @@
+
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+let namePath = path.join(__dirname, '../formsubmissions.json')
+
+
+
+app.post('/formsubmissions', (req, res, next) => {
+  let arr = [];
+  arr.push(req.body.name);
+  fs.appendFileSync(namePath, JSON.stringify(arr));
+  res.send(names);
+  next();
+})
+
+let names = fs.readFileSync(namePath, {encoding: "utf-8"});
+// console.log(names);
+
+app.get('/formsubmissions', (req, res) => {
+  
+  res.send(names);
+  
+});
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+
 
 app.use((req, res, next) => {
   console.log(req.originalUrl);
   next();
 })
 
+
+
 // app.get('/', (req, res) => {
-//   // res.send('Hello from the web server side...');
+//   res.send('Hello from the web server side...');
 //   res.sendFile(path.join(__dirname, '../public/index.html'))
 // });
 
@@ -16,11 +48,9 @@ app.use((req, res, next) => {
 //   res.sendFile(path.join(__dirname, '../public/css/styles.css'));
 // })
 
-app.use(express.static(path.join(__dirname, '../public')));
 
-// app.get('/order/:id', (req, res) => {
-//   let id = req.params.id;
-//   res.send(id);
-// })
+
+
+
 
 app.listen(3000);
