1a36ec0eaf37a3df72a2f93a13cd4fd89e5b47d8
PJ Jones <pj.jones4000.com>
2022-03-24 16:04:23 -0500

    format

--- server/server.js
+++ server/server.js
@@ -1,44 +1,36 @@
-
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 let app = express();
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-let namePath = path.join(__dirname, '../formsubmissions.json')
+let namePath = path.join(__dirname, "../formsubmissions.json");
 
 app.use((req, res, next) => {
   console.log(req.originalUrl);
   next();
-})
+});
 
-app.post('/formsubmissions', (req, res, next) => {
+app.post("/formsubmissions", (req, res, next) => {
   let arr = [];
   arr.push(req.body.name);
   fs.appendFileSync(namePath, JSON.stringify(arr));
-  let names = fs.readFileSync(namePath, {encoding: "utf-8"});
+  let names = fs.readFileSync(namePath, { encoding: "utf-8" });
   res.send(names);
   next();
-})
-
+});
 
 // console.log(names);
 
 // app.get('/formsubmissions', (req, res) => {
-  
+
 //   res.send(names);
-  
+
 // });
 
-app.use(express.static(path.join(__dirname, '../public')));
-
-
-
-
-
-
+app.use(express.static(path.join(__dirname, "../public")));
 
 // app.get('/', (req, res) => {
 //   res.send('Hello from the web server side...');
@@ -48,10 +40,5 @@ app.use(express.static(path.join(__dirname, '../public')));
 // app.get('/css/styles.css', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../public/css/styles.css'));
 // })
-
-
-
-
-
 
 app.listen(3000);
